a88266b738da23706a409d514e50561fdd81b77b
Geo Lee <geolee1@korea.ac.kr>
2024-10-17 20:00:55 +0900

    chane main

--- src/total.py
+++ src/total.py
@@ -1,6 +1,11 @@
+# module import가 안되는 문제 해결
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import audio
 import driver
-import sys, time
+import time
 
 def main(*argv, **kwargv):
     d = driver.Driver()
@@ -10,7 +15,6 @@ def main(*argv, **kwargv):
     m.play("car moving", blocking=False)
     d.driveForward(50,3)
 
-    m.play("start")
     m.play("now parking")
     d.driveBackward(50, 1, -1)
     d.driveBackward(50, 0.8, 0)
@@ -20,7 +24,7 @@ def main(*argv, **kwargv):
     time.sleep(5)
 
     m.play("leave parking")
-    m.play("now moving", blocking=False)
+    m.play("car moving", blocking=False)
     d.driveForward(50, 0.8, 1)
     d.driveForward(50, 0.8, 0)
     d.driveForward(50, 1, -1)
@@ -28,7 +32,7 @@ def main(*argv, **kwargv):
 
     time.sleep(5)
 
-    m.play("now moving", blocking=False)
+    m.play("car moving", blocking=False)
     d.driveForward(50,3.5)
     m.play("car stop")
     m.play("finish", blocking=False)
