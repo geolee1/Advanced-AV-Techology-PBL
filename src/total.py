ed22c786331ffdd512d9e2930d2d35a370fb6e1d
Geo Lee <geolee1@korea.ac.kr>
2024-10-17 22:43:32 +0900

    add wait music

--- src/total.py
+++ src/total.py
@@ -13,29 +13,37 @@ def main(*argv, **kwargv):
 
     m.play("start")
     m.play("car moving", blocking=False)
+    m.wait_finish()
     d.driveForward(50,3)
 
     m.play("now parking")
+    m.wait_finish()
     d.driveBackward(50, 1, -1)
     d.driveBackward(50, 0.8, 0)
     d.driveBackward(50, 0.8, 1)
     m.play("complete parking")
+    m.wait_finish()
 
-    time.sleep(5)
+    time.sleep(1)
 
     m.play("leave parking")
     m.play("car moving", blocking=False)
+    m.wait_finish()
     d.driveForward(50, 0.8, 1)
     d.driveForward(50, 0.8, 0)
     d.driveForward(50, 1, -1)
     m.play("complete leaving")
+    m.wait_finish()
 
-    time.sleep(5)
+    time.sleep(1)
 
     m.play("car moving", blocking=False)
+    m.wait_finish()
     d.driveForward(50,3.5)
     m.play("car stop")
     m.play("finish", blocking=False)
+    m.wait_finish()
+    
     return
 
 if __name__ == '__main__':
