44fa1f0b99a05a479edc9ec9410142fcea076287
Geo Lee <geolee1@korea.ac.kr>
2024-10-24 17:25:41 +0900

    change total move

--- src/total.py
+++ src/total.py
@@ -8,20 +8,26 @@ import driver
 import time
 
 def main(*argv, **kwargv):
-    d = driver.Driver()
     m = audio.Musicplayer()
+    d = d.Driver()
+    d.input_thread.start()
 
+
+    # 주행 명령을 추가
     m.play("start")
     m.play("car moving", terminate=False)
     m.wait_finish()
-    d.driveForward(50,3)
+    d.add_command(50, 3, 0, 'forward')   
+    d.execute_commands()
+ 
 
     m.play("now parking")
     m.play("beep effect", repeat=100, terminate=False)
     m.wait_finish()
-    d.driveBackward(50, 1, -1)
-    d.driveBackward(50, 0.8, 0)
-    d.driveBackward(50, 0.8, 1)
+    d.add_command(50, 1, -1, 'backward')
+    d.add_command(50, 0.8, 0, 'backward')
+    d.add_command(50, 0.8, 1, 'backward')
+    d.execute_commands()
     m.play("complete parking")
     m.wait_finish()
 
@@ -30,9 +36,10 @@ def main(*argv, **kwargv):
     m.play("leave parking")
     m.play("car moving", terminate=False)
     m.wait_finish()
-    d.driveForward(50, 0.8, 1)
-    d.driveForward(50, 0.8, 0)
-    d.driveForward(50, 1, -1)
+    d.add_command(50, 0.8, 1, 'forward')
+    d.add_command(50, 0.8, 0, 'forward')
+    d.add_command(50, 1, -1, 'forward')
+    d.execute_commands()
     m.play("complete leaving")
     m.wait_finish()
 
@@ -40,10 +47,13 @@ def main(*argv, **kwargv):
 
     m.play("car moving", terminate=False)
     m.wait_finish()
-    d.driveForward(50,3.5)
+    d.add_command(50, 3.5, 0, 'forward')
+    d.execute_commands()
     m.play("car stop")
     m.play("finish", terminate=False)
     m.wait_finish()
+
+    d.input_thread.join()
     
     return
 
